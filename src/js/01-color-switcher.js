f1d7fd73b77cb25a03f3b914ab4937adfd2df4e4
Anastassia-creator <118266539+Anastassia-creator@users.noreply.github.com>
2023-10-03 19:19:11 +0300

    1

---
+++ src/js/01-color-switcher.js
@@ -0,0 +1,21 @@
+const body  = document.querySelector('body');
+const start = document.querySelector('[data-start]');
+const stopt = document.querySelector('[data-stop]');
+
+start.addEventListener('click', changeColor);
+
+function changeColor (e) {
+    body.style.backgroundColor = getRandomHexColor();
+    if (e.target === start) {
+        return start.setAttribute('disabled', 'true')
+    }
+     if (e.target === stopt) {
+        return setAttribute('disabled', 'true')
+    }
+    setInterval(changeColor,1000);
+}
+
+
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
+  }
